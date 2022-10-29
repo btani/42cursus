f4c14e669811db9dad472a90f23bed9c373bd6f4
bea <105412556+btani@users.noreply.github.com>
2022-10-29 17:06:26 +0200

    Add files via upload

---
+++ Libft/ft_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btani <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 16:13:27 by btani             #+#    #+#             */
+/*   Updated: 2022/10/06 16:19:35 by btani            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned const char	*st1;
+	unsigned const char	*st2;
+	size_t				i;
+
+	st1 = s1;
+	st2 = s2;
+	i = 0;
+	while (i < n)
+	{
+		if (st1[i] != st2[i])
+			return (st1[i] - st2[i]);
+		i++;
+	}
+	return (0);
+}
