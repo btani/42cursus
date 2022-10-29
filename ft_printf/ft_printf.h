d390eba599d86b5181b6119902815245adc0743d
bea <105412556+btani@users.noreply.github.com>
2022-10-29 17:26:20 +0200

    Add files via upload

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btani <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/28 16:48:06 by btani             #+#    #+#             */
+/*   Updated: 2022/10/28 16:48:15 by btani            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <unistd.h>
+# include <stdarg.h>
+
+int		ft_countdec(int n);
+int		ft_countdec_uns(unsigned int n);
+int		ft_counthexa(unsigned long n);
+int		ft_printchar(char c);
+int		ft_printf(const char *s, ...);
+void	ft_printhexa(unsigned long n);
+int		ft_printhexa1(unsigned int n, char type);
+int		ft_printnbr(int n);
+int		ft_printstr(char *str);
+int		ft_printunsnbr(unsigned int n);
+int		ft_printvoidptr(unsigned long n);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putnbr_uns_fd(unsigned int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+#endif
---
+++ ft_printf/ft_printvoidptr.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printvoidptr.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btani <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/25 15:06:36 by btani             #+#    #+#             */
+/*   Updated: 2022/10/25 15:06:40 by btani            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ft_printf.h"
+
+int	ft_printvoidptr(unsigned long n)
+{
+	int	len;
+
+	len = 0;
+	if ((void *)n == NULL)
+		return (write(1, "(nil)", 5));
+	len += write(1, "0x", 2);
+	ft_printhexa(n);
+	len += ft_counthexa(n);
+	return (len);
+}
