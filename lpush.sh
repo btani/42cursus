e2e5f3769d50438d2e8e9961c2da5f9fee44591b
Beatrice Tani <btani@c2r1p3.42firenze.it>
2023-03-01 12:00:06 +0100

    Self Push: Wed 01 Mar 2023 12:00:06 PM CET

--- lpush.sh
+++ lpush.sh
@@ -3,4 +3,4 @@
 cd /nfs/homes/btani/Desktop/42cursus && git add .
 cd /nfs/homes/btani/Desktop/42cursus && git commit -m "Self Push: $(date)"
 cd /nfs/homes/btani/Desktop/42cursus && git push
-printf "\e[1;32mPush eseguito alle $(date | awk '{print substr($5, 1, 5)}') $(date | awk '{print $6}')\e[0m"
+printf "\e[1;32mPush eseguito alle $(date | awk '{print substr($5, 1, 5)}'; awk '{print $6}')\e[0m"
