5778321fa2fe5a3d6a7eb4cad433c260d36b3ccf
btani <beatricetnii@gmail.com>
2023-10-28 17:52:43 +0200

    Self Push: Sat 28 Oct 2023 05:52:42 PM CEST

--- lpush.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /nfs/homes/btani/Desktop/42cursus && git add .
-cd /nfs/homes/btani/Desktop/42cursus && git commit -m "Self Push: $(date)"
-cd /nfs/homes/btani/Desktop/42cursus && git push
-printf "\e[1;32mPush eseguito alle $(date | awk '{print substr($5, 1, 5), $6}')\e[0m"
