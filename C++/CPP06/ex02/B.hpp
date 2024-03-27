c19311d2183b939e6f1fe0a52270d7f9374bc165
btani <beatricetnii@gmail.com>
2024-03-27 21:35:41 +0100

    Self Push: Wed 27 Mar 2024 09:35:41 PM CET

--- C++/CPP06/ex02/B.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef B_HPP
-# define B_HPP
-# include <iostream>
-# include "Base.hpp"
-
-class B: public Base {};
-
-#endif
-//The class don’t have to be designed in the Orthodox Canonical Form.
